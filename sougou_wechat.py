c85099d7ef641ffe53ff404c44cf9c11ab75c504
AIpha00 <502553173@qq.com>
2019-07-30 17:23:00 +0800

    sougou antispider

--- sougou_wechat.py
+++ sougou_wechat.py
@@ -145,34 +145,3 @@ def get_rel_url():
 
 if __name__ == '__main__':
     snuid = get_cookie(query_url)
-    # data = {
-    #     "uigs_cl": "first_click",
-    #     "uigs_refer": "",
-    #     "uigs_t": str(int(round(time.time() * 1000))),
-    #     "uigs_productid": "vs_web",
-    #     "terminal": "web",
-    #     "vstype": "weixin",
-    #     "pagetype": "result",
-    #     "channel": "result_account",
-    #     "s_from": "",
-    #     "sourceid": "",
-    #     "type": "weixin_search_pc",
-    #     "uigs_cookie": "SUID%2Csct",
-    #     "uuid": uuid.uuid1(),
-    #     "query": seed,
-    #     "weixintype": "1",
-    #     "exp_status": "-1",
-    #     "exp_id_list": "0_0",
-    #     "wuid": "",
-    #     "snuid": snuid.split("=")[-1],
-    #     "rn": "1",
-    #     "login": "0",
-    #     "uphint": "0",
-    #     "bottomhint": "0",
-    #     "page": "1",
-    #     "exp_id": "null_0-null_1-null_2-null_3-null_4-null_5-null_6-null_7-null_8-null_9",
-    #     "time": "20429",
-    # }
-    # suv_url = "https://pb.sogou.com/pv.gif?" + urlencode(data)
-    # suv = get_suv(suv_url)
-    # print(snuid + ";" + suv)
